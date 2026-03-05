147f09ccf5942d6e54ef4da42a0a7e7fa73cc277
pandaxcore <pandadesign7@gmail.com>
2026-03-05 23:31:10 +0500

    add data to db

--- read_xlsx.py
+++ read_xlsx.py
@@ -1,15 +1,31 @@
+from datetime import datetime
 from openpyxl import load_workbook
 from engine import engine
-from model import Base
+from model import Base, People
+from sqlalchemy.orm import Session
 
 
 Base.metadata.create_all(engine)
 
 wb = load_workbook('file_example_XLS_10.xlsx')
-print(wb.sheetnames)
 
 sheet = wb['Sheet1']
-print(sheet['d10'].value)
 
-for row in sheet.iter_rows(values_only=True):
-    print(row[0])
+with Session(engine) as session:
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+
+        date_pattern = "%d/%m/%Y"
+        date_obj = datetime.strptime(row[6], date_pattern)
+
+        person = People(
+            first_name=row[1],
+            last_name=row[2],
+            gender=row[3],
+            country=row[4],
+            age=row[5],
+            date=date_obj
+        )
+
+        session.add(person)
+
+    session.commit()
